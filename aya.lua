c3443e74700e89f050b44053555d4f3b76495ee7
huychu1312 <chuhuy1312@gmail.com>
2025-08-19 11:12:58 +0700

    Update aya.lua

--- aya.lua
+++ aya.lua
@@ -282,5 +282,5 @@ getgenv().ConfigsKaitun = {
 		}
 	},
 }
-License = "dY8f40dvdVToW40xyQj7gqt8faG4IoFO"
+License = "hLv5vGDrHC1cR2eyIaPkonhV0CmU0L12"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Real-Aya/Loader/main/Init.lua'))()
