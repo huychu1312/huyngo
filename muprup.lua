574769a931238312c48d30b6522377a88c2f54e4
huychu1312 <chuhuy1312@gmail.com>
2025-11-05 11:50:48 +0700

    Update muprup.lua

--- muprup.lua
+++ muprup.lua
@@ -1,9 +1,10 @@
 getgenv().Config = {
+    SERVER_HOP = false, -- true: bật hop server, false: tắt hop server
     DPS_THRESHOLDS = {
-     HOUSE1 = { '>0' }, -- mua theo dps , buy by dps
+        HOUSE1 = { '>0' },
         HOUSE2 = { '>1000' },
         HOUSE3 = { '6000~14000', '>25000' },
-        HOUSE4 = { '14000~24000','>35000' },
+        HOUSE4 = { '14000~24000', '>35000' },
         HOUSE5 = { '>24000' },
         HOUSE6 = { '>30000' },
     },
@@ -15,18 +16,20 @@ getgenv().Config = {
         Usernames = { 'zaocancelo' }, --- name1 , name2 , name3 ---
         PetSendInterval = 20,
         SEND_ALL = true, -- true: SEND ALL, false: SEND PET CONFIG ----
-        Name_Pet = { 'Huge Skeleton Snake', 'Huge Specter Owl', 'Huge Shark Cat' }, --HUGE NAME 1 , HUGE NAME 2 , HUGE NAME 3 ---
+        Name_Pet = {}, --HUGE NAME 1 , HUGE NAME 2 , HUGE NAME 3 ---
     },
     SEND_DIAMONDS = {
         Usernames = { 'NguyenVanThuan47' }, --- name1 , name2 , name3 ---
-        MinDiamonds = 100000000,
+        MinDiamonds = 10000000,
     },
     SEND_ITEM = {
         Usernames = { 'NguyenVanThuan47' }, --- name1 , name2 , name3 ---
+        ['Spectral Potion'] = { amount = 3 }, --- number and 'all' ----
     },
     SEND_EGGS = {
         Usernames = { 'NguyenVanThuan47' }, --- name1 , name2 , name3 ---
         ['Exclusive Egg 56'] = true,
     },
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7c082a1ad968228f9d5e0d6eb722ca19.lua"))()
+script_key="kPbAfLkZIoRTkZMFwvOiMlkCFhplgxNC";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b9b3f5defc507615a3da8cb6e859d3cd.lua"))()
