bf316b3f34ab5f8a578465f03d1eb4b0a60afa8e
huychu1312 <chuhuy1312@gmail.com>
2025-12-12 17:11:22 +0700

    Update fish huy.lua

--- fish huy.lua
+++ fish huy.lua
@@ -1,6 +1,11 @@
 _G.FishItConfig = _G.FishItConfig or {
     ["Fishing"] = {
         ["Auto Perfect"] = true,
+        ["Auto Favorite"] = false,
+        ["Fish Name"] = {
+            "Gar Fish",
+        },
+        ["Auto Accept Trade"] = true,
     },
     ["Auto Trade"] = {
         ["Enabled"] = false,
@@ -22,7 +27,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     ["Doing Quest"] = {
         ["Auto Ghostfinn Rod"] = true,
-        ["Auto Element Rod"] = false,
+        ["Auto Element Rod"] = true,
         ["Allowed Sacrifice"] = {
             "Blob Shark",
             "Thin Armor Shark",
@@ -56,15 +61,29 @@ _G.FishItConfig = _G.FishItConfig or {
             "Shark Hunt",
         },
     },
+     ["Potions"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Astral Rod",
+    },
     ["Enchant"] = {
         ["Auto Enchant"] = true,
-        ["Roll Enchant"] = false, -- Roll enchant until get Leprechaun II
-        ["Minimum Rod"] = "Astral Rod",
+        ["Roll Enchant"] = false,
         ["Enchant List"] = {
             "Leprechaun II",
             "Mutation Hunter II",
             "Prismatic I",
         },
+        ["Second Enchant"] = true,
+        ["Allowed Sacrifice"] = {
+            "Blob Shark",
+            "Skeleton Narwhal",
+        },
+        ["Second Enchant List"] = {
+            "Reeler I",
+            "Prismatic I",
+            "Mutation Hunter II",
+        },
+        ["Minimum Rod"] = "Ghostfinn Rod",
     },
     ["Bait List"] = {
         ["Auto Buying"] = true,
