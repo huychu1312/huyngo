f344adc87538779e86fa3b02c3515a98fea2f695
huychu1312 <chuhuy1312@gmail.com>
2026-01-01 21:54:51 +0700

    Update fish huy.lua

--- fish huy.lua
+++ fish huy.lua
@@ -1,57 +1,138 @@
-script_key = "hcmVKYKuzxAojTdphLPoQiSfDtRlfdVK";
-task.wait(60)
-getgenv().GGFX_MODE = 1 -- or 2 to if you want to see what happens
-getgenv().GDO_XMAS_WORLD = true
-getgenv().GHOP_ON_STAFF = true
-getgenv().GEVENT_UPGRADES = {
-"MegaPresentGrowFaster",
-"MegaPresentMoreLoot",
-"MegaPresentEggLuckBoost",
---"MegaPresentHigherHugeChance",
-"MegaPresentHigherTitanicChance",
-"MegaPresentHigherGargantuanChance"
+_G.FishItConfig = _G.FishItConfig or {
+    ["Fishing"] = {
+        ["Auto Perfect"] = true,
+        ["Auto Favorite"] = true,
+        ["Fish Name"] = {
+            "Sacred Guardian Squid",
+        },
+        ["Auto Accept Trade"] = true,
+    },
+    ["Auto Trade"] = {
+        ["Enabled"] = false,
+        ["Whitelist Username"] = {""},
+        ["Category Fish"] = {
+            "Secret",
+        },
+        ["Fish Name"] = {
+        },
+        ["Item Name"] = {
+            "Evolved Enchant Stone",
+        },
+    },
+    ["Farm Coin Only"] = {
+        ["Enabled"] = false, -- Farm coins only [ cant buy rod, bait, enchant, weather ]
+        ["Target"] = 190000,
+    },
+    ["Selling"] = {
+        ["Auto Sell"] = true,
+        ["Auto Sell Threshold"] = "Legendary",
+        ["Auto Sell Every"] = 100,
+    },
+    ["Doing Quest"] = {
+        ["Auto Ghostfinn Rod"] = true,
+        ["Auto Element Rod"] = false,
+        ["Unlock Ancient Ruin"] = true,
+        ["Allowed Sacrifice"] = {
+            "Blob Shark",
+            "Ghost Shark",
+        },
+        ["FARM_LOC_SECRET_SACRIFICE"] = "Ocean",
+
+        ["Minimum Rod"] = "Astral Rod",
+    },
+    ["WebHook"] = {
+        ["Link Webhook"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
+        ["Auto Sending"] = false,
+        ["Category"] = {"Secret"},
+
+        ["Link Webhook Quest Complete"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
+    },
+    ["Weather"] = {
+        ["Auto Buying"] = false,
+        ["Minimum Rod"] = "Astral Rod",
+        ["Weather List"] = {
+            "Wind",
+        },
+    },
+    ["Potions"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Astral Rod",
+    },
+    ["Totems"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Ghostfinn Rod",
+        ["Buy List"] = {
+            "Luck Totem",
+            "Mutation Totem",
+            "Shiny Totem",
+        },
+    },
+    ["Event"] = {
+        ["Start Farm"] = true,
+        ["Minimum Rod"] = "Ghostfinn Rod",
+        ["Event List"] = {
+            ["Christmas Cave"] = false,
+            ["Hacker Event"] = true,
+            "Megalodon Hunt",
+            "Ghost Shark Hunt",
+            "Shark Hunt",
+        },
+    },
+    ["Enchant"] = {
+        ["Auto Enchant"] = true,
+        ["Roll Enchant"] = false, -- true = Roll until get TOP 1 enchant only (first in list)
+        ["Evolved Roll Enchant"] = true, -- true = Roll Evolved Stone until get ANY evolved-only enchant from list, then fallback to regular stone
+        ["Enchant List"] = {
+            "SECRET Hunter",
+        },
+        ["Second Enchant"] = false,
+        ["Allowed Sacrifice"] = {
+            "Frostborn Shark",
+            "Cryoshade Glider",
+        },
+        ["Second Enchant List"] = {
+            "Reeler I",
+            "Prismatic I",
+            "Mutation Hunter II",
+        },
+        ["Minimum Rod"] = "Ghostfinn Rod",
+    },
+    ["Bait List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {
+            "Midnight Bait",
+            "Chroma Bait",
+            "Corrupt Bait",
+            "Aether Bait",
+        "Singularity Bait",
+            "1x1x1x1 Bait",
+        },
+    },
+    ["Rod List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {
+            "Grass Rod",
+            "Midnight Rod",
+            "Astral Rod",
+            "Ares Rod",
+            "Angler Rod",
+        },
+        ["Location Rods"] = {
+            ["Fisherman Island"] = {"Starter Rod"},
+            ["Kohana Volcano"] = {"Grass Rod", "Midnight Rod"},
+            ["Tropical Grove"] = {"Astral Rod"},
+            ["Ancient Ruin"] = {"Element Rod", "Ghostfinn Rod", "Angler Rod", "Ares Rod"},
+        },
+    },
+
+    ["ExtremeFpsBoost"] = true,
+    ["UltimatePerformance"] = true,
+    ["Disable3DRender"] = true,
+    ["AutoRemovePlayer"] = true,
+    ["AutoReconnect"] = true,
+    ["HideGUI"] = false,
+    ["Debug"] = true,
+    ["EXIT_MAP_IF_DISCONNECT"] = false,
 }
-getgenv().GXMAS_WORLD_DO_ICE_BLOCKS = true
-getgenv().GXMAS_WORLD_MIN_HATCH_REQUESTS_PER_CYCLE = 25
-getgenv().GXMAS_WORLD_MIN_FARM_TIME_SECONDS = 300
-getgenv().GXMAS_WORLD_CRAFT_SANTA_GIFTS = false -- if you put to true it cannot buy the upgrades as they also use santa hats
-getgenv().GXMAS_WORLD_DO_MEGA_PRESENT = false
-getgenv().GDO_ADVENT_CALENDAR = true
-getgenv().GXMAS_WORLD_AUTO_FAVORITE_BEST_PETS = true
-getgenv().GCLEAR_FAVORITED_PETS = true
-getgenv().GDO_LOCKPICKING = false
-getgenv().GXMAS_WORLD_ENTER_RAFFLE = true
-getgenv().GXMAS_WORLD_FOCUS_EGGS_FOR_AUTO_CRAFT_PETS = true
-getgenv().GXMAS_WORLD_DO_GINGERBREAD_CHEST = true
-getgenv().GXMAS_WORLD_DO_SNOWMAN_BOSS_CHEST = true
-getgenv().GXMAS_WORLD_OPEN_MEGA_PRESENT_IF_LUCK_REACH = 8000
-getgenv().GXMAS_WORLD_PETS_EXCLUDED_FROM_SENDING = {"Elf Golem","Candycane Kitsune", "Hippomint", "Krampus","Merry Mule"}
-getgenv().GRANK_FIRST = false --if you use this you have to define GREBIRTH_TO and GRANK_TO
-getgenv().GZONE_TO = 1 -- to enter event from W1 put 1, from W2 put 100, from W3 200, from W4 240
-getgenv().GUSE_ULTIMATES = {"Tornado","Hidden Treasure","Pet Surge","Tsunami","Black Hole","UFO","Nightmare","Lightning Storm","TNT Shower","Chest Spell","Ground Pound"}
-getgenv().GPOTIONS = {"Coins","Lucky","Treasure Hunter","Walkspeed","Diamonds","Damage"}
-getgenv().GENCHANTS = {"Tap Power", "Coins", "Treasure Hunter", "Strong Pets", "Criticals", "Diamonds"}
-getgenv().GPOTIONS_MAX_TIER = 99
-getgenv().GWEBHOOK_USERID = "993062998652493824"
-getgenv().GWEBHOOK_LINK = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r"
-getgenv().GHUGE_COUNT = 0 -- amount of huges to keep/not mail
-getgenv().GMAIL_RECEIVERS = {"NguyenVanThuan47"}
-getgenv().GMAIL_ITEMS = {
-["All my Elf Golem"] = {Class = "Pet", Id = "Elf Golem", MinAmount = 2, AllVariants = true},
-["All my Candycane Kitsune"] = {Class = "Pet", Id = "Candycane Kitsune", MinAmount = 2, AllVariants = true},
-["All my Hippomint"] = {Class = "Pet", Id = "Hippomint", MinAmount = 2, AllVariants = true},
-["All my Krampus"] = {Class = "Pet", Id = "Krampus", MinAmount = 5, AllVariants = true},
-["All my Merry Mule"] = {Class = "Pet", Id = "Merry Mule", MinAmount = 10, AllVariants = true},
-["All Huges"] = {Class = "Pet", Id = "All Huges", MinAmount = 1},
-["Ice Cube egg"] = {Class = "Egg", Id = "Exclusive Egg 58", MinAmount = 1},
-["Send Diamonds"] = {Class = "Currency", Id = "Diamonds", KeepAmount = "5m", MinAmount = "50m"}, -- mail diamonds, to enable lower MinAmount..
-["Hype Egg 2"] = {Class = "Lootbox", Id = "Hype Egg 2", MinAmount = 1},
-["Daycare egg 5"] = {Class = "Egg", Id = "Huge Machine Egg 5", MinAmount = 1},
-["Candycane Gift"] = {Class = "Lootbox", Id = "Candycane Gift", MinAmount = 36},
-["Santa Gift"] = {Class = "Lootbox", Id = "Santa Gift", MinAmount = 36},
-["Cove Lockpick"] = {Class = "Misc", Id = "Lockpick A", MinAmount = 36},
-["Gold Lockpick"] = {Class = "Misc", Id = "Lockpick B", MinAmount = 5},
-["Diamond Lockpick"] = {Class = "Misc", Id = "Lockpick B", MinAmount = 1},
-}
-getgenv().GMAX_MAIL_COST = "2m"
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ba2dcad2127dcfc04301dfe52ce6c61c.lua"))()
+script_key="CDD0AEE7DAD2093FD6A6917901B02022";
+local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)() 
