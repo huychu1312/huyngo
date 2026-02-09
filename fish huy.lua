fb0d88f855c9fe5943d32ebeb5e505352e75032a
huychu1312 <chuhuy1312@gmail.com>
2026-02-09 22:47:06 +0700

    Update fish huy.lua

--- fish huy.lua
+++ fish huy.lua
@@ -1,142 +1,68 @@
-task.spawn(function()
-    while true do
-        setfpscap(3)
-        task.wait(10)
-    end
-end)
-_G.FishItConfig = _G.FishItConfig or {
-    ["Fishing"] = {
-        ["Auto Perfect"] = true,
-        ["Auto Favorite"] = true,
-        ["Fish Name"] = {
-            "Sacred Guardian Squid",
-        },
-        ["Auto Accept Trade"] = true,
-    },
-    ["Auto Trade"] = {
-        ["Enabled"] = false,
-        ["Whitelist Username"] = {""},
-        ["Category Fish"] = {
-            "Secret",
-        },
-        ["Fish Name"] = {
-        },
-        ["Item Name"] = {
-            "Evolved Enchant Stone",
-        },
-    },
-    ["Farm Coin Only"] = {
-        ["Enabled"] = false, -- Farm coins only [ cant buy rod, bait, enchant, weather ]
-        ["Target"] = 190000,
-    },
-    ["Selling"] = {
-        ["Auto Sell"] = true,
-        ["Auto Sell Threshold"] = "Legendary",
-        ["Auto Sell Every"] = 100,
-    },
-    ["Doing Quest"] = {
-        ["Auto Ghostfinn Rod"] = true,
-        ["Auto Element Rod"] = false,
-        ["Unlock Ancient Ruin"] = false,
-        ["Allowed Sacrifice"] = {
-            "Blob Shark",
-            "Ghost Shark",
-        },
-        ["FARM_LOC_SECRET_SACRIFICE"] = "Ocean",
+script_key="QLeTKCfDmciNPdpwJRFeUzrWlmYgbTME";
+setfpscap(3)
+getgenv().tapSimConfig = {
+    -- Event Config
+    CRAFT_EASY_KEY = true,
+    CRAFT_HARD_KEY = true,
+    OPEN_GALAXY_LUCKY_BLOCK = true,
+    CREATE_LUCKY_BLOCK = { "Weather Lucky Block", "Wisdom Lucky Block", "Starry Lucky Block", "Totem Lucky Block", "Lucky Block" },
+    USE_LUCKY_BLOCK = { "Weather Lucky Block", "Wisdom Lucky Block", "Starry Lucky Block", "Totem Lucky Block", "Lucky Block" },
+    FIND_ALL_BLOCK_ESSENCE = true,
 
-        ["Minimum Rod"] = "Astral Rod",
-    },
-    ["WebHook"] = {
-        ["Link Webhook"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
-        ["Auto Sending"] = true,
-        ["Category"] = {"Secret"},
+    -- General Config
+    AUTO_UPDATE_RESTART = true,
+    STABLE_HATCH = false,  -- uses game hatch delay
+    AUTO_EQUIP_BEST = true,
+    ADD_FRIEND = true,
+    MAX_REBIRTH = math.huge,
+    REBIRTH_DELAY = 120,  -- Seconds
+    REDEEM_CODE = { "russo", "lucky", "tacos", "enchant", "HTH" },
+    OPEN_EGG = { "Cyberpunk", "Lucky Event" },
+    UNLOCK_WORLD_2_BEST_EGG = false,  -- change true if farm best egg, false if event egg
 
-        ["Link Webhook Quest Complete"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
-    },
-    ["Weather"] = {
-        ["Auto Buying"] = false,
-        ["Minimum Rod"] = "Astral Rod",
-        ["Weather List"] = {
-            "Wind",
-        },
-    },
-    ["Potions"] = {
-        ["Auto Use"] = true,
-        ["Minimum Rod"] = "Astral Rod",
-    },
-    ["Totems"] = {
-        ["Auto Use"] = true,
-        ["Minimum Rod"] = "Ghostfinn Rod",
-        ["Buy List"] = {
-            "Luck Totem",
-            "Mutation Totem",
-            "Shiny Totem",
-        },
-    },
-    ["Event"] = {
-        ["Start Farm"] = true,
-        ["Minimum Rod"] = "Ghostfinn Rod",
-        ["Event List"] = {
-            ["Christmas Cave"] = false,
-            ["Hacker Event"] = true,
-            "Megalodon Hunt",
-            "Ghost Shark Hunt",
-            "Shark Hunt",
-        },
-    },
-    ["Enchant"] = {
-        ["Auto Enchant"] = true,
-        ["Roll Enchant"] = true, -- true = Roll until get TOP 1 enchant only (first in list)
-        ["Evolved Roll Enchant"] = true, -- true = Roll Evolved Stone until get ANY evolved-only enchant from list, then fallback to regular stone
-        ["Enchant List"] = {
-            "SECRET Hunter",
-        },
-        ["Second Enchant"] = false,
-        ["Allowed Sacrifice"] = {
-            "Frostborn Shark",
-            "Cryoshade Glider",
-        },
-        ["Second Enchant List"] = {
-            "Reeler I",
-            "Prismatic I",
-            "Mutation Hunter II",
-        },
-        ["Minimum Rod"] = "Ghostfinn Rod",
-    },
-    ["Bait List"] = {
-        ["Auto Buying"] = true,
-        ["Buy List"] = {
-            "Midnight Bait",
-            "Chroma Bait",
-            "Corrupt Bait",
-            "Aether Bait",
-        "Singularity Bait",
-            "1x1x1x1 Bait",
-        },
-    },
-    ["Rod List"] = {
-        ["Auto Buying"] = true,
-        ["Buy List"] = {
-            "Grass Rod",
-            "Midnight Rod",
-            "Astral Rod",
-            "Ares Rod",
-            "Angler Rod",
-        },
-        ["Location Rods"] = { -- Farm location and minimum rod to start on that location, also only 1 rod and only 1 location
-            ["Fisherman Island"] = {"Starter Rod"}, -- Do not change
-            ["Kohana Volcano"] = {"Grass Rod", "Midnight Rod"},
-            ["Tropical Grove"] = {"Astral Rod"},
-        ["Ocean"] = {"Element Rod", "Ghostfinn Rod", "Angler Rod", "Ares Rod"},
-        },
+    BUY_GAMEPASS = { "SecretHunter", "MagicEggs", "x8Egg", "+4Equip", "+2Equip", "SuperLucky", "FasterEgg", "Luck", "Storage2", "Storage1" },
+    BUY_AUTO_CLICKER = true,
+    OPEN_ALL_GIFT = true,
+    BUY_MERCHANT = {},
+    BUY_GEM_SHOP = { 
+        ["RebirthButtons"] = 34, 
+        ["ClickMultiplier"] = 20, 
+        ["HatchSpeed"] = 5, 
+        ["GoldenLuck"] = 6, 
+        ["CriticalChance"] = 5, 
+        ["AutoClickerSpeed"] = 0, 
+        ["DoubleJumps"] = 0 
     },
 
-    ["ExtremeFpsBoost"] = true,
-    ["UltimatePerformance"] = true,
-    ["Disable3DRender"] = true,
-    ["AutoRemovePlayer"] = false,
-    ["AutoReconnect"] = false,
-    ["Debug"] = true,
-}
-script_key="CDD0AEE7DAD2093FD6A6917901B02022";
-local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)() 
+    ENCHANT_EQUIPPED_ONLY = true,
+    AUTO_ENCHANT = { 
+        ["Secret I"] = { "Secret Hunter" },
+        ["Secret II"] = { "Secret Hunter" },
+        ["Secret III"] = { "Secret Hunter" },
+    },
+    FALLBACK_3_SECRET_HUNTER_ENCHANT = { 
+        ["Secret I"] = { "Rainbow Hunter" },
+        ["Secret II"] = { "Rainbow Hunter" },
+        ["Secret III"] = { "Rainbow Hunter" },
+    },
+
+    AUTO_DELETE_EGG_PET = { 
+        ["Cyberpunk"] = { "Cyber Bear", "Cyber Deer", "Future Cat", "Evil Future Cat", "Mechanical Eye" },
+        ["Lucky Event"] = { "Yellow Luckyblock", "Blue Luckyblock", "Lucky Fairy", "Lucky Angel" },
+    },
+    KEEP_PET_NAME = { }, 
+    KEEP_PET_RARITY = { "Secret I", "Secret II", "Secret III", "Secret X" },
+    GOLDEN_PET = { ["Prismatic Luckyblock"] = 6 },  -- ["Pet Name"] = 6 
+    RAINBOW_PET = { ["Prismatic Luckyblock"] = 5 },  -- ["Pet Name"] = 5
+    USE_ALL_POTIONS = true,
+    USE_BOOST = { },  -- "Boost Name"
+    USE_TOTEM = { "TotemOfClicks", "TotemOfHatchSpeed", "TotemOfLuck", "TotemOfRebirths", "TotemOfSecretLuck" },
+
+    -- Webhook
+    WEBHOOK_PET_RARITY = { "Secret I", "Secret II", "Secret III", "Secret X" },
+    WEBHOOK_URL = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
+    DISCORD_ID = "993062998652493824",
+    SHOW_PUBLIC_DISCORD_ID = true,
+    SHOW_WEBHOOK_USERNAME = true,
+    SHOW_WEBHOOK_JOBID = true,
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ffab66f7d96a48c13b12e526b19b37d4.lua"))()
