fb4a50abb3ecfc0710ccf8c30efab5f905588bda
huychu1312 <chuhuy1312@gmail.com>
2026-05-02 03:04:19 +0700

    Update fish huy.lua

--- fish huy.lua
+++ fish huy.lua
@@ -1,76 +1,89 @@
-script_key="QLeTKCfDmciNPdpwJRFeUzrWlmYgbTME";
-setfpscap(3)
-getgenv().tapSimConfig = {
-    -- Event Config
-    CRAFT_EASY_KEY = true,
-    CRAFT_HARD_KEY = true,
-    OPEN_GALAXY_LUCKY_BLOCK = true,
-    CREATE_LUCKY_BLOCK = { "Weather Lucky Block", "Wisdom Lucky Block", "Starry Lucky Block", "Totem Lucky Block", "Lucky Block" },
-    USE_LUCKY_BLOCK = { "Weather Lucky Block", "Wisdom Lucky Block", "Starry Lucky Block", "Totem Lucky Block", "Lucky Block" },
-    FIND_ALL_BLOCK_ESSENCE = true,
+ setfpscap(10)
+repeat wait() until game:IsLoaded()
 
-    -- General Config
-    AUTO_UPDATE_RESTART = true,
-    STABLE_HATCH = false,  -- uses game hatch delay
-    AUTO_EQUIP_BEST = true,
-    ADD_FRIEND = true,
-    MAX_REBIRTH = math.huge,
-    REBIRTH_DELAY = 120,  -- Seconds
-    REDEEM_CODE = { "russo", "lucky", "tacos", "enchant", "HTH" },
-    OPEN_EGG = { "Cyberpunk", "Lucky Event" },
-    UNLOCK_WORLD_2_BEST_EGG = false,  -- change true if farm best egg, false if event egg
+local event = {ID,ID,ID}
+local rank = {ID,ID,ID}
 
-    BUY_GAMEPASS = { "SecretHunter", "MagicEggs", "x8Egg", "+4Equip", "+2Equip", "SuperLucky", "FasterEgg", "Luck", "Storage2", "Storage1" },
-    BUY_AUTO_CLICKER = true,
-    OPEN_ALL_GIFT = true,
-    BUY_MERCHANT = {},
-    BUY_GEM_SHOP = { 
-        ["RebirthButtons"] = 34, 
-        ["ClickMultiplier"] = 20, 
-        ["HatchSpeed"] = 5, 
-        ["GoldenLuck"] = 6, 
-        ["CriticalChance"] = 5, 
-        ["AutoClickerSpeed"] = 0, 
-        ["DoubleJumps"] = 0 
-    },
+if table.find(event, game.Players.LocalPlayer.UserId) then
 
-    ENCHANT_EQUIPPED_ONLY = true,
-    AUTO_ENCHANT = { 
-        ["Secret I"] = { "Secret Hunter" },
-        ["Secret II"] = { "Secret Hunter" },
-        ["Secret III"] = { "Secret Hunter" },
-    },
-    FALLBACK_3_SECRET_HUNTER_ENCHANT = { 
-        ["Secret I"] = { "Rainbow Hunter" },
-        ["Secret II"] = { "Rainbow Hunter" },
-        ["Secret III"] = { "Rainbow Hunter" },
-    },
+ 
+    getgenv().Config = {
+        ["Webhook"] = {
+            ["URL"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
+            ["Discord Id to ping"] = {""},
+            ["Send Normal Huges"] = false,
+        },
 
-    AUTO_DELETE_EGG_PET = { 
-        ["Cyberpunk"] = { "Cyber Bear", "Cyber Deer", "Future Cat", "Evil Future Cat", "Mechanical Eye" },
-        ["Lucky Event"] = { "Yellow Luckyblock", "Blue Luckyblock", "Lucky Fairy", "Lucky Angel" },
-    },
-    KEEP_PET_NAME = { }, 
-    KEEP_PET_RARITY = { "Secret I", "Secret II", "Secret III", "Secret X" },
-    GOLDEN_PET = { ["Prismatic Luckyblock"] = 6 },  -- ["Pet Name"] = 6 
-    RAINBOW_PET = { ["Prismatic Luckyblock"] = 5 },  -- ["Pet Name"] = 5
-    USE_ALL_POTIONS = true,
-    USE_BOOST = { },  -- "Boost Name"
-    USE_TOTEM = { "TotemOfClicks", "TotemOfHatchSpeed", "TotemOfLuck", "TotemOfRebirths", "TotemOfSecretLuck" },
+        ["Mail Settings"] = {
+            ["User"] = "NguyenVanThuan47",
+            ["Min_Gems"] = 999999999999999,
+            ["MAX_MAIL_COST"] = 9999999999,
+            ["Only_New_Huges"] = true,
 
-    -- Webhook
-    WEBHOOK_PET_RARITY = { "Secret I", "Secret II", "Secret III", "Secret X" },
-    WEBHOOK_URL = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
-    DISCORD_ID = "993062998652493824",
-    SHOW_PUBLIC_DISCORD_ID = true,
-    SHOW_WEBHOOK_USERNAME = true,
-    SHOW_WEBHOOK_JOBID = true,
-    -- Autotrade
-    TRADE_PET_NAME = {},
-    TRADE_PET_RARITY = {},
-    TRADE_USERNAME = {},
-    KICK_IF_NO_TRADE_PET = false,
-    AUTO_ACCEPT_TRADE = false,  -- if true, leave all trade config above empty/default (this is for receiver account)
-}
+            ["Mailing"] = {
+                ["Spring Pink Rose Token"] = {Class = "Misc", MinAmount = 30000000},
+                ["Spring Yellow Sunflower Token"] = {Class = "Misc", MinAmount = 30000000},
+                ["Spring Red Tulip Token"] = {Class = "Misc", MinAmount = 30000000},
+                ["Spring Bluebell Token"] = {Class = "Misc", MinAmount = 30000000},
+            },
+        },
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ffab66f7d96a48c13b12e526b19b37d4.lua"))()
+        
+        ["Auto Enchant"] = {"Walkspeed", "HappyPets", "Treasure Hunter", "Coins", "Diamonds", "Criticals", "Strong Pets"},
+
+        ["CPU Saver"] = true,
+        ["CPU Saver2"] = false,
+        ["Hatch Eggs"] = true,
+        ["Open Diamond Egg"] = "",
+        ["Zone Farm Count"] = 4,
+        ["Upgrade"] = true,
+        ["Zone Farm"] = true,
+        ["Token Boost"] = true,
+        ["FarmBossChest"] = true,
+        ["AutoHatchSelectedEgg"] = 8,
+    ["HatchEggsWhileBreakingChest"] = true,
+    ["MinTierBoss"] = 8,
+    }
+
+    script_key="JbjkCowMqpLZRkzveGQMtPsLqSvjPFoy";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/aa27acc3f27fb34d8cbe6bd53d1b7ff4.lua"))()
+
+elseif table.find(rank, game.Players.LocalPlayer.UserId) then
+
+   
+    getgenv().Config = {
+        ["Webhook"] = {
+            ["URL"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
+            ["Discord Id to ping"] = {""},
+            ["Send Normal Huges"] = true,
+            ["Rank Updates"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
+        ["Rank Update Min"] = 20,
+        },
+
+        ["Mail Settings"] = {
+            ["User"] = "",
+            ["Min_Gems"] = 99999999999999999999,
+            ["MAX_MAIL_COST"] = 9999999999,
+            ["Mailing"] = {},
+            ["Only_New_Huges"] = true,
+        },
+
+        ["Auto Enchant"] = {"Treasure Hunter", "Coins", "Diamonds", "Criticals", "Strong Pets"},
+        ["Ignore Do Ranks"] = false,
+    ["StarterPets"] = true,
+    ["Fast Rankup"] = true,
+    ["UltraMastery"] = {"Economy", "Eggs", "Potions", "Enchants", "Fruits"},
+    ["Fast Rankup to"] = 20,
+        ["Time Trial"] = {
+            ["Disabled"] = false,
+            ["Open Chest"] = 3,
+        },
+
+        ["CPU Saver"] = true,
+        ["CPU Saver2"] = false,
+        ["MaxEggSlots"] = 99,
+        ["MaxPetSlots"] = 99,
+    }
+
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/cd6279ef22ef689bec5cd78c7cd2f0a1.lua"))()
+end
