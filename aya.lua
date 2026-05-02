8be1607f241503a80708bf3187322d4fae5a30aa
huychu1312 <chuhuy1312@gmail.com>
2026-05-02 23:10:05 +0700

    Update aya.lua

--- aya.lua
+++ aya.lua
@@ -1,122 +1,43 @@
-_G.FishItConfig = _G.FishItConfig or {
-    ["Fishing"] = {
-        ["Auto Perfect"] = true,
-        ["Auto Favorite"] = false,
-        ["Fish Name"] = {
-            "Gar Fish",
-        },
-        ["Auto Accept Trade"] = true,
-    },
-    ["Auto Trade"] = {
-        ["Enabled"] = false,
-        ["Whitelist Username"] = {""},
-        ["Category Fish"] = { -- Only Select one, fill in the fish category or fish name
-            "Secret",
-        },
-        ["Fish Name"] = {
-        },
-    },
-    ["Farm Coin Only"] = {
-        ["Enabled"] = false, -- Farm coins only [ cant buy rod, bait, enchant, weather ]
-        ["Target"] = 190000,
-    },
-    ["Selling"] = {
-        ["Auto Sell"] = true,
-        ["Auto Sell Threshold"] = "Legendary",
-        ["Auto Sell Every"] = 100,
-    },
-    ["Doing Quest"] = {
-        ["Auto Ghostfinn Rod"] = true,
-        ["Auto Element Rod"] = false,
-        ["Allowed Sacrifice"] = {
-            "Blob Shark",
-            "Thin Armor Shark",
-            "Ghost Shark",
-            "Cryoshade Glider",
-        },
-        ["FARM_LOC_SECRET_SACRIFICE"] = "Christmas Island",
-
-        ["Minimum Rod"] = "Astral Rod",
-    },
-    ["WebHook"] = {
-        ["Link Webhook"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
-        ["Auto Sending"] = true,
-        ["Category"] = {"Secret"},
-
-        ["Link Webhook Quest Complete"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
-    },
-    ["Weather"] = {
-        ["Auto Buying"] = true,
-        ["Minimum Rod"] = "Ghostfinn Rod",
-        ["Weather List"] = {
-            "Wind",
-        },
-    },
-     ["Potions"] = {
-        ["Auto Use"] = true,
-        ["Minimum Rod"] = "Astral Rod",
-    },
-    ["Event"] = {
-        ["Start Farm"] = true,
-        ["Minimum Rod"] = "Ghostfinn Rod",
-        ["Event List"] = {
-            "Megalodon Hunt",
-            "Ghost Shark Hunt",
-            "Shark Hunt",
-        },
-    },
-   ["Enchant"] = {
-        ["Auto Enchant"] = true,
-        ["Roll Enchant"] = false,
-        ["Enchant List"] = {
-            "Leprechaun II",
-        },
-        ["Second Enchant"] = false,
-        ["Allowed Sacrifice"] = {
-            "Blob Shark",
-            "Skeleton Narwhal",
-        },
-        ["Second Enchant List"] = {
-            "Reeler I",
-            "Prismatic I",
-            "Mutation Hunter II",
-        },
-        ["Minimum Rod"] = "Ghostfinn Rod",
-    },
-    ["Bait List"] = {
-        ["Auto Buying"] = true,
-        ["Buy List"] = {
-            "Midnight Bait",
-            "Chroma Bait",
-            "Corrupt Bait",
-            "Aether Bait",
-        "Singularity Bait",
-            "1x1x1x1 Bait",
-        },
-    },
-    ["Rod List"] = {
-        ["Auto Buying"] = true,
-        ["Buy List"] = {
-            "Grass Rod",
-            "Midnight Rod",
-            "Astral Rod",
-            "Ares Rod",
-            "Angler Rod",
-        },
-        ["Location Rods"] = { -- Farm location and minimum rod to start on that location, also only 1 rod and only 1 location
-            ["Fisherman Island"] = {"Starter Rod"}, -- Do not change
-            ["Kohana Volcano"] = {"Grass Rod", "Midnight Rod"},
-            ["Tropical Grove"] = {"Astral Rod"},
-        ["Ocean"] = {"Element Rod", "Ghostfinn Rod", "Angler Rod", "Ares Rod"},
-        },
+setfpscap(10)
+repeat wait() until game:IsLoaded()
+getgenv().Config = {
+    ["Webhook"] = {
+        ["URL"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
+        ["Discord Id to ping"] = {""},
+        ["Send Normal Huges"] = false,
     },
 
-    ["ExtremeFpsBoost"] = true,
-    ["UltimatePerformance"] = true,
-    ["Disable3DRender"] = true,
-    ["AutoRemovePlayer"] = true,
-    ["AutoReconnect"] = false,
-    ["Debug"] = true,
+    ["Mail Settings"] = {
+            ["User"] = "NguyenVanThuan47",
+            ["Min_Gems"] = 999999999999999,
+            ["MAX_MAIL_COST"] = 9999999999,
+            ["Only_New_Huges"] = true,
+            ["Mailing"] = {
+                ["Spring Pink Rose Token"] = {Class = "Misc", MinAmount = 30000000},
+                ["Spring Yellow Sunflower Token"] = {Class = "Misc", MinAmount = 30000000},
+                ["Spring Red Tulip Token"] = {Class = "Misc", MinAmount = 30000000},
+                ["Spring Bluebell Token"] = {Class = "Misc", MinAmount = 30000000},
+            },
+        },
+    ["CPU Saver"] = true,
+    ["Auto Enchant"] = {"Walkspeed", "HappyPets", "Treasure Hunter", "Coins", "Diamonds", "Criticals", "Strong Pets"},
+    -- Egg Hatching Settings
+    ["Hatch Eggs"] = true,  -- Enable egg hatching
+    ["AutoHatchSelectedEgg"] = 8,
+
+    -- Upgrade Settings
+    ["Upgrade"] = true,  -- Automatically purchase egg upgrades when possible
+
+    -- Zone Farming Settings
+    ["Zone Farm"] = true,  -- Enable zone farming
+    ["Zone Farm Count"] = 4,  -- Number of zones to farm simultaneously (max 4)
+    ["Open Diamond Egg"] = "",  -- Specific diamond egg to open (e.g., "Spring Diamond Egg 100x"), leave empty to disable
+    -- Boss Chest Settings
+    ["FarmBossChest"] = true, -- this will farm the boss chest 
+    ["HatchEggsWhileBreakingChest"] = true, -- this will tp away from the boss chest and it will then open eggs in the mean while
+    ["MinTierBoss"] = 8, -- it will not farm boss chest that are lower then this, so for me only 8 will farm
+
+    ["Token Boost"] = true  -- Automatically add time to token boosts
 }
-script_key="CDD0AEE7DAD2093FD6A6917901B02022";
-local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)()    
+script_key="JbjkCowMqpLZRkzveGQMtPsLqSvjPFoy";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/aa27acc3f27fb34d8cbe6bd53d1b7ff4.lua"))()
