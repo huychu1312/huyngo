531d80c100be8c44d4e2f83d6951df5d8eb00af0
huychu1312 <chuhuy1312@gmail.com>
2026-05-08 22:31:57 +0700

    Update fishduong2.lua

--- fishduong2.lua
+++ fishduong2.lua
@@ -1,36 +1,76 @@
-getgenv().Config = {
-    ["PetPen"] = {
-        ["EggToBuy"] = "Cracked Egg",
-    },
-    ["PetReleaser"] = {
-        ["RaritiesToMakeNeon"] = {"common", "uncommon", "rare", "ultra_rare", "legendary"},
-        ["RaritiesToMakeMega"] = {},
+setfpscap(10)
+repeat wait() until game:IsLoaded()
+getgenv().Config =  {
+    ["Webhook"] = {
+        ["URL"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
+        ["Discord Id to ping"] = {"993062998652493824"},
+        ["Send Normal Huges"] = false,
 
-        ["RaritiesToRelease"] = {"common", "uncommon", "rare", "ultra_rare", "legendary"}
+        ["Rank Updates"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
+        ["Rank Update Min"] = 20 -- will send rank update when reaches that ranl
     },
-    ["AutoTrade"] = {
-        ["TradeUsers"] = {
-            ""
-        },
-        ["Pets"] = {
-             "Silverback Gorilla","Balloon Unicorn","Purrowl", "Dragonfruit Fox", "Dango Penguins", "Emberlight", "Frostbite Bear", "Diamond Unicorn", "Sea Turtle", "Glormy Crab"
+    ["Mail Settings"] = {
+        ["User"] = "NguyenVanThuan47",
+        ["Min_Gems"] = 100000000,
+        ["MAX_MAIL_COST"] = 2000000,
+        ["Only_New_Huges"] = false,
+
+        ["Mailing"] = {
+            ["Special Ops Egg"] = {Class = "Egg", MinAmount = 1},
+            ["Gift Bag"] = {Class = "Misc", MinAmount = 99},
+            ["Diamond Gift Bag"] = {Class = "Misc", MinAmount = 99},
+            ["Large Gift Bag"] = {Class = "Misc", MinAmount = 99},
+            ["Charm Stone"] = {Class = "Misc", MinAmount = 50},
+            ["Rainbow Mini Chest"] = {Class = "Misc", MinAmount = 10},
+            ["Mini Chest"] = {Class = "Misc", MinAmount = 50},
+            ["Pinata"] = {Class = "Misc", MinAmount = 50},
+            ["Exotic Treasure Flag"] = {Class = "Misc", MinAmount = 50},
+            ["Diamond Plant Seed"] = {Class = "Misc", MinAmount = 99},
+            ["Insta-Plant Capsule"] = {Class = "Misc", MinAmount = 99},
+            ["MVP Key Lower Half"] = {Class = "Misc", MinAmount = 50},
+            ["MVP Key Upper Half"] = {Class = "Misc", MinAmount = 50},
+            ["MVP Key"] = {Class = "Misc", MinAmount = 50},
+            ["Veil Horse"] = {Class = "Pet", MinAmount = 50},
+            ["Wicked Kirin"] = {Class = "Pet", MinAmount = 50},
+            ["Fantasy Key"] = {Class = "Misc", MinAmount = 50},
+            ["Secret Key"] = {Class = "Misc", MinAmount = 50},
+            ["Void Key"] = {Class = "Misc", MinAmount = 50},
+            ["Tech Key"] = {Class = "Misc", MinAmount = 50},
+            ["Time Trial Gift #2"] = {Class = "Misc", MinAmount = 10},
+            ["Hype Egg #2"] = {Class = "Misc", MinAmount = 1},
+            ["Basketball Gift"] = {Class = "Misc", MinAmount = 50},
+            ["Candycane Gift"] = {Class = "Misc", MinAmount = 50},
+            ["Snowflake Gift"] = {Class = "Misc", MinAmount = 50},
+            ["Cosmic Basketball Gift"] = {Class = "Misc", MinAmount = 50},
+            ["Santa Gift"] = {Class = "Misc", MinAmount = 50},
+            ["Blossom Gift"] = {Class = "Misc", MinAmount = 50},
+            ["Overload Charm"] = {Class = "Misc", MinAmount = 1},
         }
     },
-    ["AutoChangeConfig"] = {
-        ["CrystalEggAmount"] = 1000000000,
-        ["TradeablePets"] = 1000000000,
-        ["CollectServers"] = {
-            "",
-        }
+    ["Auto Enchant"] = {"Treasure Hunter", "Coins", "Diamonds", "Criticals", "Strong Pets"},
+    ["Time Trial"] = {
+        ["Disabled"] = false, -- will disable Timetrial
+        ["Open Chest"] = 3, -- 1,2,3 or do false to not open
     },
-    ["BuyItemWithTickets"] = "Crystal Egg",
-    ["OpenCrystalEggs"] = true,
-    ["ConvertBucksToTickets"] = true,
-    ["AutoBuyJiggly"] = true,
-    ["AutoBuyMochi"] = true,
-    ["UseAgeUpPotions"] = true,
-    ["OpenAdminAbuseEggs"] = true,
-    ["KeepTickets"] = false,
-    ["WebhookURL"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
+
+    ["CPU Saver"] = true, -- makes your game run smoother
+
+    ["MaxEggSlots"] = 99,
+    ["MaxPetSlots"] = 99,
+
+    ["Ignore Do Ranks"] = false, -- wont do quest at all
+
+    ["Fast Rankup"] = false, -- will focus on quest in world 1 until it reaches a specific rank (broken a bit)
+    ["Fast Rankup to"] = 20, -- choose your rank you want to do
+    ["UltraMastery"] = {"Economy", "Eggs", "Potions", "Enchants", "Fruits"},
+
+    ["StarterPets"] = true, -- will hatch event pets
+    ["ClaimFreeStuff"] = false, -- disables claiming stuff loop (may fix stuck on upgrading stuff)
+    ["AutoCombinePresents"] = true, -- combines Prestes to the desired level
+    ["MaxCombineTier"] = 4, -- 1 = max Medium, 2 = max Large, 3 = max X-Large, 4 = max Titanic (default, craft everything)
+    ["OpenPresent"] = 5, -- 1 = small, 2 = Medium, 3 = Large, 4 = X-Large, 5 = Titanic, false = disabled
+
+    ["AutoConsumeFlag"] = {"Strength Flag","Hasty Flag"},
+    ["AutoSprinkler"] = true,
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9af1f22af0378cb275bfeac9c6971ba5.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/cd6279ef22ef689bec5cd78c7cd2f0a1.lua"))()
