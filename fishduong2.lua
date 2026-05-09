0a7134d38735de94e5ef93370399108c9b1e1a9b
huychu1312 <chuhuy1312@gmail.com>
2026-05-10 01:25:42 +0700

    Update fishduong2.lua

--- fishduong2.lua
+++ fishduong2.lua
@@ -1,13 +1,10 @@
-setfpscap(5)
+setfpscap(3)
 repeat wait() until game:IsLoaded()
 getgenv().Config =  {
     ["Webhook"] = {
         ["URL"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
         ["Discord Id to ping"] = {"993062998652493824"},
         ["Send Normal Huges"] = true,
-
-        ["Rank Updates"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
-        ["Rank Update Min"] = 20 -- will send rank update when reaches that ranl
     },
     ["Mail Settings"] = {
         ["User"] = "NguyenVanThuan47",
@@ -47,30 +44,8 @@ getgenv().Config =  {
             ["Overload Charm"] = {Class = "Misc", MinAmount = 1},
         }
     },
-    ["Auto Enchant"] = {"Treasure Hunter", "Coins", "Diamonds", "Criticals", "Strong Pets"},
-    ["Time Trial"] = {
-        ["Disabled"] = false, -- will disable Timetrial
-        ["Open Chest"] = 3, -- 1,2,3 or do false to not open
-    },
-
-    ["CPU Saver"] = true, -- makes your game run smoother
-
-    ["MaxEggSlots"] = 99,
-    ["MaxPetSlots"] = 99,
-
-    ["Ignore Do Ranks"] = false, -- wont do quest at all
-
-    ["Fast Rankup"] = false, -- will focus on quest in world 1 until it reaches a specific rank (broken a bit)
-    ["Fast Rankup to"] = 20, -- choose your rank you want to do
-    ["UltraMastery"] = {"Economy", "Eggs", "Potions", "Enchants", "Fruits"},
-
-    ["StarterPets"] = true, -- will hatch event pets
-    ["ClaimFreeStuff"] = false, -- disables claiming stuff loop (may fix stuck on upgrading stuff)
-    ["AutoCombinePresents"] = true, -- combines Prestes to the desired level
-    ["MaxCombineTier"] = 4, -- 1 = max Medium, 2 = max Large, 3 = max X-Large, 4 = max Titanic (default, craft everything)
-    ["OpenPresent"] = 5, -- 1 = small, 2 = Medium, 3 = Large, 4 = X-Large, 5 = Titanic, false = disabled
-
-    ["AutoConsumeFlag"] = {"Strength Flag","Hasty Flag"},
-    ["AutoSprinkler"] = true,
+    ["CPU Saver"] = true,
+    ["AutoUseDice"] = "Mega Lucky Dice V2",
+    ["PetsToSell"] = {"Sand Turtle", "Sprout Stag", "Blossom Fox", "Honey Bee", "Chamelon", "Tiedye Elephant", "Surge Wolf", "Starry Eye Wolf", "Bubble Bunny", "Sapphire Dragon", "Wisp Owl", "Mystical Kitsune"},
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/cd6279ef22ef689bec5cd78c7cd2f0a1.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9f9b656e577ba1b9d87d4c855e7ac1a4.lua"))()
