5c9dde141da31c926b1a3d41db737013db44deb0
huychu1312 <chuhuy1312@gmail.com>
2026-05-19 05:59:52 +0700

    Update fishduong2.lua

--- fishduong2.lua
+++ fishduong2.lua
@@ -1,54 +1,30 @@
-setfpscap(5)
-repeat wait() until game:IsLoaded()
-getgenv().Config =  {
-    ["Webhook"] = {
-        ["URL"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
-        ["Discord Id to ping"] = {"993062998652493824"},
-        ["Send Normal Huges"] = true,
-    },
-    ["Mail Settings"] = {
-        ["User"] = "NguyenVanThuan47",
-        ["Min_Gems"] = 100000000,
-        ["MAX_MAIL_COST"] = 2000000,
-        ["Only_New_Huges"] = false,
+pcall(function()
+    setfpscap(15)
+end)
 
-        ["Mailing"] = {
-            ["Exclusive Special Ops Egg"] = {Class = "Egg", MinAmount = 1},
-            ["Exclusive Origami Egg"] = {Class = "Egg", MinAmount = 1},
-            ["Mystical Kitsune"] = {Class = "Pet", MinAmount = 1},
-            ["Wisp Owl"] = {Class = "Pet", MinAmount = 1},
-            ["Gift Bag"] = {Class = "Misc", MinAmount = 99},
-            ["Diamond Gift Bag"] = {Class = "Misc", MinAmount = 99},
-            ["Large Gift Bag"] = {Class = "Misc", MinAmount = 99},
-            ["Charm Stone"] = {Class = "Misc", MinAmount = 50},
-            ["Rainbow Mini Chest"] = {Class = "Misc", MinAmount = 10},
-            ["Mini Chest"] = {Class = "Misc", MinAmount = 50},
-            ["Pinata"] = {Class = "Misc", MinAmount = 50},
-            ["Exotic Treasure Flag"] = {Class = "Misc", MinAmount = 50},
-            ["Diamond Plant Seed"] = {Class = "Misc", MinAmount = 99},
-            ["Insta-Plant Capsule"] = {Class = "Misc", MinAmount = 99},
-            ["MVP Key Lower Half"] = {Class = "Misc", MinAmount = 50},
-            ["MVP Key Upper Half"] = {Class = "Misc", MinAmount = 50},
-            ["MVP Key"] = {Class = "Misc", MinAmount = 50},
-            ["Fantasy Key"] = {Class = "Misc", MinAmount = 50},
-            ["Secret Key"] = {Class = "Misc", MinAmount = 50},
-            ["Void Key"] = {Class = "Misc", MinAmount = 50},
-            ["Tech Key"] = {Class = "Misc", MinAmount = 50},
-            ["Time Trial Gift #2"] = {Class = "Misc", MinAmount = 10},
-            ["Hype Egg #2"] = {Class = "Misc", MinAmount = 1},
-            ["Exclusive Raffle Ticket"] = {Class = "Misc", MinAmount = 1},
-            ["Basketball Gift"] = {Class = "Misc", MinAmount = 50},
-            ["Candycane Gift"] = {Class = "Misc", MinAmount = 50},
-            ["Snowflake Gift"] = {Class = "Misc", MinAmount = 50},
-            ["Cosmic Basketball Gift"] = {Class = "Misc", MinAmount = 50},
-            ["Santa Gift"] = {Class = "Misc", MinAmount = 50},
-            ["Blossom Gift"] = {Class = "Misc", MinAmount = 50},
-            ["Overload Charm"] = {Class = "Misc", MinAmount = 1},
-        }
+task.wait(30)
+getgenv().Configuration = {
+    ["Webhook"] = {
+        ["UserID"]    = "993062998652493824",
+        ["WebhookURL"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
+        ['Ping Global'] = true, -- wont ping but it will show discord
     },
-    ["CPU Saver"] = true,
-    ["AutoUseDice"] = {"Mega Lucky Dice V2"},
-    ["SyncOnWeather"] = false,
-    ["PetsToSell"] = {"Sand Turtle", "Sprout Stag", "Blossom Fox", "Honey Bee", "Chamelon", "Tiedye Elephant", "Surge Wolf", "Starry Eye Wolf", "Bubble Bunny", "Sapphire Dragon", "Wisp Owl", "Mystical Kitsune"},
+    ['Mail'] = {
+        ['Usernames']      = {""},
+        ['Mail All Huges'] = true,
+        ['Items']          = {},
+    },
+
+    ["Dice On Bonus Only"] = true,
+    ['Fire On Weather Only'] = true,
+
+    ["Craft Dice"]  = "Mega Lucky Dice II V2",
+    ["Use Dice"]    = true,
+    ['Upgrades']    = true,
+    ['Sell Pets']   = {},
+
+    ['Potions']  = { "Lucky Eggs", "Damage Potion", "Treasure Hunter" },
+    ['Enchants'] = { "Criticals", "Criticals", "Lucky Eggs", "Lucky Eggs", "Lucky Eggs" },
+    ['Fruits']   = { "Rainbow Fruit", "Orange", "Watermelon", "Apple", "Banana", "Pineapple" },
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9f9b656e577ba1b9d87d4c855e7ac1a4.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/8e09351860576c626242d87609b01594.lua"))()
