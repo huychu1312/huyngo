39b652fe16d27a88bcc1976091c49ff77dcd0cea
huychu1312 <chuhuy1312@gmail.com>
2026-07-12 11:32:05 +0700

    Update fish huy.lua

--- fish huy.lua
+++ fish huy.lua
@@ -7,7 +7,7 @@ getgenv().Config =  {
         ["Send Normal Huges"] = true,
 
         ["Rank Updates"] = "https://discord.com/api/webhooks/1394039615949639822/ru1nhLaHQ0YheuaUN238iKf337NATfBXg224aTMSInFTWhNg24i7aPpFSIy6NAUAfr3r",
-        ["Rank Update Min"] = 20 -- will send rank update when reaches that ranl
+        ["Rank Update Min"] = 8 -- will send rank update when reaches that ranl
     },
     ["Mail Settings"] = {
         ["User"] = "NguyenVanThuan47",
@@ -16,35 +16,6 @@ getgenv().Config =  {
         ["Only_New_Huges"] = false,
 
         ["Mailing"] = {
-            ["Exclusive Special Ops Egg"] = {Class = "Egg", MinAmount = 1},
-            ["Exclusive Origami Egg"] = {Class = "Egg", MinAmount = 1},
-            ["Gift Bag"] = {Class = "Misc", MinAmount = 99},
-            ["Diamond Gift Bag"] = {Class = "Misc", MinAmount = 99},
-            ["Large Gift Bag"] = {Class = "Misc", MinAmount = 99},
-            ["Charm Stone"] = {Class = "Misc", MinAmount = 50},
-            ["Rainbow Mini Chest"] = {Class = "Misc", MinAmount = 10},
-            ["Mini Chest"] = {Class = "Misc", MinAmount = 50},
-            ["Pinata"] = {Class = "Misc", MinAmount = 50},
-            ["Exotic Treasure Flag"] = {Class = "Misc", MinAmount = 50},
-            ["Diamond Plant Seed"] = {Class = "Misc", MinAmount = 99},
-            ["Insta-Plant Capsule"] = {Class = "Misc", MinAmount = 99},
-            ["MVP Key Lower Half"] = {Class = "Misc", MinAmount = 50},
-            ["MVP Key Upper Half"] = {Class = "Misc", MinAmount = 50},
-            ["MVP Key"] = {Class = "Misc", MinAmount = 50},
-            ["Fantasy Key"] = {Class = "Misc", MinAmount = 50},
-            ["Secret Key"] = {Class = "Misc", MinAmount = 50},
-            ["Void Key"] = {Class = "Misc", MinAmount = 50},
-            ["Tech Key"] = {Class = "Misc", MinAmount = 50},
-            ["Time Trial Gift #2"] = {Class = "Misc", MinAmount = 10},
-            ["Hype Egg #2"] = {Class = "Misc", MinAmount = 1},
-            ["Exclusive Raffle Ticket"] = {Class = "Misc", MinAmount = 1},
-            ["Basketball Gift"] = {Class = "Misc", MinAmount = 50},
-            ["Candycane Gift"] = {Class = "Misc", MinAmount = 50},
-            ["Snowflake Gift"] = {Class = "Misc", MinAmount = 50},
-            ["Cosmic Basketball Gift"] = {Class = "Misc", MinAmount = 50},
-            ["Santa Gift"] = {Class = "Misc", MinAmount = 50},
-            ["Blossom Gift"] = {Class = "Misc", MinAmount = 50},
-            ["Overload Charm"] = {Class = "Misc", MinAmount = 1},
         }
     },
     ["Auto Enchant"] = {"Treasure Hunter", "Coins", "Diamonds", "Criticals", "Strong Pets"},
@@ -60,7 +31,7 @@ getgenv().Config =  {
 
     ["Ignore Do Ranks"] = false, -- wont do quest at all
 
-    ["Fast Rankup"] = false, -- will focus on quest in world 1 until it reaches a specific rank (broken a bit)
+    ["Fast Rankup"] = true, -- will focus on quest in world 1 until it reaches a specific rank (broken a bit)
     ["Fast Rankup to"] = 20, -- choose your rank you want to do
     ["UltraMastery"] = {"Economy", "Eggs", "Potions", "Enchants", "Fruits"},
 
